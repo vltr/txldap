1f5e9a87e35bbeca2ca2ff44403273dcfc8b08b6
Silas Sewell <silas@sewell.ch>
2011-01-21 20:48:28 -0500

    Fix various issues and add debug option

--- txldap.py
+++ txldap.py
@@ -1,10 +1,18 @@
+# Copyright (c) 2010, Silas Sewell
+# All rights reserved.
+#
+# This file is subject to the MIT License (see LICENSE file).
+
 from ldap import *
 from twisted.internet import threads
 
 class Connection(object):
 
     def __init__(self, uri, *args, **kwargs):
+        self.debug = kwargs.pop('debug', False)
         self._ldap = initialize(uri, *args, **kwargs)
+        if self.debug:
+            self._ldap.set_option(ldap.OPT_DEBUG_LEVEL, 255)
 
     def add(self, *args, **kwargs):
         return threads.deferToThread(self._ldap.add_ext_s, *args, **kwargs)
@@ -19,26 +27,26 @@ class Connection(object):
         else:
             return threads.deferToThread(self._ldap.bind_s, who, cred, method)
 
-    def compare(self, *args, **kargs):
+    def compare(self, *args, **kwargs):
         return threads.deferToThread(self._ldap.compare_ext_s, *args, **kwargs)
 
-    def delete(self, *args, **kargs):
+    def delete(self, *args, **kwargs):
         return threads.deferToThread(self._ldap.delete_ext_s, *args, **kwargs)
 
-    def modify(self, *args, **kargs):
+    def modify(self, *args, **kwargs):
         return threads.deferToThread(self._ldap.modify_ext_s, *args, **kwargs)
 
-    def passwd(self, *args, **kargs):
+    def passwd(self, *args, **kwargs):
         return threads.deferToThread(self._ldap.passwd_s, *args, **kwargs)
 
-    def rename(self, *args, **kargs):
+    def rename(self, *args, **kwargs):
         return threads.deferToThread(self._ldap.rename_s, *args, **kwargs)
 
     def sasl_interactive_bind(self, *args, **kwargs):
         return threads.deferToThread(self._ldap.sasl_interactive_bind_s, *args, **kwargs)
 
     def start_tls(self, *args, **kwargs):
-        return threads.deferToThread(self._ldap.start_tls, *args, **kwargs)
+        return threads.deferToThread(self._ldap.start_tls_s, *args, **kwargs)
 
     def search(self, *args, **kwargs):
         return threads.deferToThread(self._ldap.search_s, *args, **kwargs)
